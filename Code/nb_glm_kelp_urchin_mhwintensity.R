cd1a87c4d5877cb817c581c0a3b3bf3a227d6a8b
Jiaxin Shi <117341476+jiaxins1028@users.noreply.github.com>
2025-12-26 16:53:18 +1100

    Add files via upload

---
+++ Code/nb_glm_kelp_urchin_mhwintensity.R
@@ -0,0 +1,214 @@
+# title: "nb_glm_kelp_urchin_mhwintensity"
+# author: "Jiaxin Shi"
+# date: "2025-09-27"
+
+## Loading packages
+
+library(ggplot2)
+library(gamair)
+library(dplyr)
+library(mgcv)
+library(statmod) 
+library(nls.multstart)
+library(broom)
+library(tidyverse)
+library(pracma)
+library(qgam)
+library(lme4)
+library(lmerTest)
+library(MASS)     #for negative bin regression
+library(pscl)     #for zero inflated models & predprob
+library(reshape2) #convert wide to tall
+library(AER) 
+library(visreg)
+library(patchwork)
+library(stringr) 
+
+
+## plotting theme
+
+theme_format <- theme_bw()+
+  theme(axis.text.x  = element_text(vjust=0.5,size=13, colour = "black"))+
+  theme(axis.text.y  = element_text(size=15, colour = "black"))+
+  theme(axis.title.x = element_text(size=18, colour = "black"))+
+  theme(axis.title.y = element_text(size=18, colour = "black"))+
+  theme(axis.ticks = element_line(colour="black"))+
+  theme(panel.grid.minor=element_blank())+
+  theme(panel.grid.major=element_blank())+
+  theme(strip.text = element_text(size = 15))+
+  theme(legend.text = element_text(size = 15),  
+        legend.title = element_text(size = 15))
+
+
+
+## importing data and levels
+
+# Read the data
+data <- read.csv("all_ukmhw_with_level.csv")
+data <- data %>% filter(location %in% c('Jervis Bay', 'Batemans')) 
+
+thresh_1 <- mean(data$inten_level1)
+thresh_2 <- mean(data$inten_level2)
+thresh_3 <- mean(data$inten_level3)
+
+hist(data$survey_mean)
+plot(data$number, data$survey_mean)
+
+
+## fitting glm.nb for MHW levels
+
+data_warm <- data %>% filter(summer_inten > thresh_2 & summer_inten < thresh_3-1)  
+data_warm$level <- 'Level II'
+data_moderate <- data %>% filter(summer_inten > thresh_1+0.3 & summer_inten <= thresh_2)
+data_moderate$level <- 'Level I'
+data_cool <- data %>% filter(summer_inten < thresh_1-0.5)
+data_cool$level <- 'Level 0'
+
+
+fit_nb_warm <- MASS::glm.nb(survey_mean ~ number, data = data_warm)
+summary(fit_nb_warm)
+
+fit_nb_moderate <- MASS::glm.nb(survey_mean ~ number, data = data_moderate)
+summary(fit_nb_moderate)
+
+fit_nb_cool <- MASS::glm.nb(survey_mean ~ number, data = data_cool)
+summary(fit_nb_cool)
+
+
+## Data regression and CI
+
+alpha <- 0.05  # 95% confidence interval
+z_value <- qnorm(1 - alpha/2)  # Z-score for 95% CI
+
+
+# For Level 3 (Warm)
+new_data_warm <- expand.grid(number = seq(min(data_warm$number), max(data_warm$number), 0.05))
+preds_warm <- predict(fit_nb_warm, new_data_warm, type = "link", se.fit = TRUE)
+
+new_data_warm$fit <- preds_warm$fit
+new_data_warm$se.fit <- preds_warm$se.fit
+
+new_data_warm$preds <- exp(new_data_warm$fit)
+new_data_warm$lower <- exp(new_data_warm$fit - 1.96 * new_data_warm$se.fit)
+new_data_warm$upper <- exp(new_data_warm$fit + 1.96 * new_data_warm$se.fit)
+new_data_warm$level <- 'Level II'
+
+# For Level 2 (Moderate)
+new_data_moderate <- expand.grid(number = seq(min(data_moderate$number), max(data_moderate$number), 0.05))
+preds_moderate <- predict(fit_nb_moderate, new_data_moderate, type = "link", se.fit = TRUE)
+
+new_data_moderate$fit <- preds_moderate$fit
+new_data_moderate$se.fit <- preds_moderate$se.fit
+
+new_data_moderate$preds <- exp(new_data_moderate$fit)
+new_data_moderate$lower <- exp(new_data_moderate$fit - 1.96 * new_data_moderate$se.fit)
+new_data_moderate$upper <- exp(new_data_moderate$fit + 1.96 * new_data_moderate$se.fit)
+new_data_moderate$level <- 'Level I'
+
+# For Level 1 (Cool)
+new_data_cool <- expand.grid(number = seq(min(data_cool$number), max(data_cool$number), 0.05))
+preds_cool <- predict(fit_nb_cool, new_data_cool, type = "link", se.fit = TRUE)
+
+new_data_cool$fit <- preds_cool$fit
+new_data_cool$se.fit <- preds_cool$se.fit
+
+new_data_cool$preds <- exp(new_data_cool$fit)
+new_data_cool$lower <- exp(new_data_cool$fit - 1.96 * new_data_cool$se.fit)
+new_data_cool$upper <- exp(new_data_cool$fit + 1.96 * new_data_cool$se.fit)
+new_data_cool$level <- 'Level 0'
+
+
+## Find threshold
+
+threshold <- 10  # Replace with your desired threshold value
+
+warm_threshold <- round(new_data_warm[new_data_warm$preds <= threshold, "number"], 1)
+moderate_threshold <- round(new_data_moderate[new_data_moderate$preds <= threshold, "number"], 1)
+cool_threshold <- round(new_data_cool[new_data_cool$preds <= threshold, "number"], 1)
+
+print(warm_threshold[1])
+print(moderate_threshold[1])
+print(cool_threshold[1])
+
+
+## Plotting
+
+# Define text labels and their positions
+text_labels <- data.frame(
+  x = c(warm_threshold[1], moderate_threshold[1], cool_threshold[1]),  # x positions
+  y = c( 0,  0,  0),  # y positions, adjust as needed
+  label = c(warm_threshold[1], moderate_threshold[1], cool_threshold[1]),
+  level = c('Level II', 'Level I', 'Level 0'))
+
+plot_JB <- ggplot() +
+  labs(x= expression('Sea urchin density /m'^2), y="Percentage Canopy Cover/%", title = '')+
+  geom_point(data = data_warm, aes(x=number, y=survey_mean, color = level), shape=16, size = 3, alpha = 0.5)+  #
+  geom_point(data = data_moderate, aes(x=number, y=survey_mean, color = level), size = 3, alpha = 0.5)+  #
+  geom_point(data = data_cool, aes(x=number, y=survey_mean, color = level), size = 3, alpha = 0.5)+  #
+  geom_line(data = new_data_warm, aes(x = number, y = preds, color = level), linewidth = 2) +
+  geom_line(data = new_data_moderate, aes(x = number, y = preds, color = level), linewidth = 2) +
+  geom_line(data = new_data_cool, aes(x = number, y = preds, color = level), linewidth = 2)+
+  geom_ribbon(data = new_data_warm, aes(x = number, ymin = lower, ymax = upper, fill = level), alpha = 0.2) +
+  geom_ribbon(data = new_data_moderate, aes(x = number, ymin = lower, ymax = upper, fill = level), alpha = 0.2) +
+  geom_ribbon(data = new_data_cool, aes(x = number, ymin = lower, ymax = upper, fill = level), alpha = 0.2) +
+  geom_hline(yintercept = threshold, linetype = "dashed", color = "dimgray", linewidth = 1) +  # Add horizontal line
+  geom_text(data = text_labels, aes(x = x+0.05, y = y, label = label, color = level), vjust = 1.5, size = 5, fontface = "bold") +  # Add text labels
+  geom_segment(data = text_labels, aes(x = x, xend = x, y = y, yend = threshold, color = level), alpha = 0.5,linewidth = 1.2) +  # Add vertical line
+  annotate("text", x = 0.5, y = threshold, label = threshold, size = 5, color = "black", fontface = "bold", hjust = 2.7)+
+  scale_color_manual(values = c(
+    'Level 0' = '#1E8E99FF',
+    'Level I' = '#FF8E32FF',
+    'Level II' = '#950404FF')) +
+  scale_fill_manual(values = c('Level 0' = '#1E8E99FF', 'Level I' = '#FF8E32FF', 'Level II' = '#950404FF')) +  # Match fill colors with line colors
+  labs(color = "Summer Max Intensity") +
+  scale_y_continuous(limits=c(0,100), expand = c(0, 0)) +
+  scale_x_continuous(limits=c(0,10), expand = c(0, 0)) +
+  theme_format+
+  theme(legend.position = c(.97, .97),
+        legend.justification = c("right", "top"),
+        legend.box.just = "right",
+        axis.text.x = element_text(margin = margin(t = 5)),  # Increase the top margin to move text down
+        axis.ticks.length.x = unit(10, "pt"),  # Increase tick length
+        legend.box.background = element_rect(color="black", linewidth=2))+
+  coord_cartesian(clip = "off") +
+  guides(color = guide_legend(override.aes = list(linetype = 1, size = 1, shape = NA)),
+         fill = FALSE)
+
+plot_JB
+
+
+## Hypothesis Test on Interaction: likelihood ratio test (LRT)
+```{r pressure, echo=FALSE}
+# Compare with a model without interaction
+model1 <- glm.nb(survey_mean ~ number + summer_inten, data = data); summary(model1)
+model2 <- glm.nb(survey_mean ~ number * summer_inten, data = data); summary(model2)
+
+anova(model1, model2, test = "Chisq")  
+
+
+
+## Confidence Interval extraction for each curve 
+
+threshold <- 10  
+
+warm_threshold <- round(new_data_warm[new_data_warm$preds <= threshold, "number"], 1)
+warm_threshold_lower <- round(new_data_warm[new_data_warm$lower <= threshold, "number"], 1)
+warm_threshold_upper <- round(new_data_warm[new_data_warm$upper <= threshold, "number"], 1)
+
+moderate_threshold <- round(new_data_moderate[new_data_moderate$preds <= threshold, "number"], 1)
+moderate_threshold_lower <- round(new_data_moderate[new_data_moderate$lower <= threshold, "number"], 1)
+moderate_threshold_upper <- round(new_data_moderate[new_data_moderate$upper <= threshold, "number"], 1)
+
+cool_threshold <- round(new_data_cool[new_data_cool$preds <= threshold, "number"], 1)
+cool_threshold_lower <- round(new_data_cool[new_data_cool$lower <= threshold, "number"], 1)
+cool_threshold_upper <- round(new_data_cool[new_data_cool$upper <= threshold, "number"], 1)
+
+# Combine in a nice table
+threshold_table <- data.frame(
+  Level   = c("Warm","Moderate","Cool"),
+  Estimate= c(warm_threshold[1], moderate_threshold[1], cool_threshold[1]),
+  LowerCI = c(warm_threshold_lower[1], moderate_threshold_lower[1], cool_threshold_lower[1]),
+  UpperCI = c(warm_threshold_upper[1], moderate_threshold_upper[1], cool_threshold_upper[1])
+)
+
+threshold_table
